dacb59cc2acff0803c11d849660400cf84639964
cagatay-usta <cagatay_usta@windowslive.com>
2023-09-02 23:00:38 +0300

    experiment with graphql data fetching, remix Link component and collection displays

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,8 @@
+import {Link, useLoaderData} from '@remix-run/react';
+import {Image} from '@shopify/hydrogen';
+import {Collection} from '@shopify/hydrogen/storefront-api-types';
+import {LoaderArgs} from '@shopify/remix-oxygen';
+
 export function meta() {
   return [
     {title: 'Hydrogen'},
@@ -5,10 +10,57 @@ export function meta() {
   ];
 }
 
+export async function loader({context}: LoaderArgs) {
+  return await context.storefront.query(COLLECTIONS_QUERY);
+}
+
 export default function Index() {
+  const {collections} = useLoaderData();
   return (
-    <div>
-      <h3>Hello from the home page!</h3>
-    </div>
+    <section className="w-full gap-4">
+      <h2 className="whitespace-pre-wrap max-w-prose font-bold text-lead">
+        Collections
+      </h2>
+      <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-3">
+        {collections.nodes.map((collection: Collection) => {
+          return (
+            <Link to={`/collections/${collection.handle}`} key={collection.id}>
+              <div className="grid gap-4">
+                {collection?.image && (
+                  <Image
+                    alt={`Image of ${collection.title}`}
+                    data={collection.image}
+                    key={collection.id}
+                    sizes="(max-width: 32em) 100vw, 33vw"
+                    crop="center"
+                  />
+                )}
+                <h2 className="whitespace-pre-wrap max-w-prose font-medium text-copy">
+                  {collection.title}
+                </h2>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
   );
 }
+
+const COLLECTIONS_QUERY = `#graphql
+  query FeaturedCollections {
+    collections(first: 3, query: "collection_type:smart") {
+      nodes {
+        id
+        title
+        handle
+        image {
+          altText
+          width
+          height
+          url
+        }
+      }
+    }
+  }
+`;
